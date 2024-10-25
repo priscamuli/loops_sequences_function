c7bfb02def34fcff2bf0b91b229e1b7801da58c7
priscamuli <priscakamanthe31@gmail.com>
2024-10-25 18:14:08 +0300

    added fizzbuzz program

---
+++ Question 1.py
@@ -0,0 +1,11 @@
+#fizzbuzz program
+#loop from 1 to 50
+for num in range(1,51):
+    if num % 3 ==0 and num % 5 == 0:
+        print("FizzBuzz")
+    elif num % 3 ==0:
+        print("Fizz")
+    elif num % 5 == 0:
+        print("Buzz")
+    else:
+        print(num)
